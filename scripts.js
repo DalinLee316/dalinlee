8415d66cc21cb115476a31337dd8339059ef3576
DalinLee316 <crane316285@gmail.com>
2024-08-19 22:32:32 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,34 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Получение элементов
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("openModal");
+var span = document.getElementsByClassName("close")[0];
+
+if (modal && btn && span) {
+    // Открытие модального окна при нажатии на кнопку
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // Закрытие модального окна при нажатии на крестик
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // Закрытие модального окна при клике на затемнённую область
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+
