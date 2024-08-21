4a44c455dd37d10d35cc69e7d88553c78e2e5a8c
DalinLee316 <crane316285@gmail.com>
2024-08-21 23:05:59 +0200

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Плавный скролл при клике на ссылки с якорем
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
@@ -7,28 +8,6 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-// Получение элементов
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("openModal");
-var span = document.getElementsByClassName("close")[0];
-
-if (modal && btn && span) {
-    // Открытие модального окна при нажатии на кнопку
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    // Закрытие модального окна при нажатии на крестик
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // Закрытие модального окна при клике на затемнённую область
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-}
+// Код для модального окна был удален
 
 
