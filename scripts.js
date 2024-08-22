0996a41ac282fb9762e13fd3c6d19eda02fb85c2
DalinLee316 <crane316285@gmail.com>
2024-08-22 20:41:43 +0200

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,5 @@
 // Открытие и закрытие модального окна
 document.querySelector('.animated-link').addEventListener('click', function(event) {
     event.preventDefault(); // Предотвращаем переход по ссылке
-    document.getElementById('projects-modal').style.display = 'block';
-});
-
-document.querySelector('.close').addEventListener('click', function() {
-    document.getElementById('projects-modal').style.display = 'none';
-});
-
-window.addEventListener('click', function(event) {
-    if (event.target === document.getElementById('projects-modal')) {
-        document.getElementById('projects-modal').style.display = 'none';
-    }
+    document.querySelector('#projects').scrollIntoView({ behavior: 'smooth' }); // Плавный переход к секции проектов
 });
